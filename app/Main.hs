7562636d086253aa5b5ef5b6f7adbd2d8fc321ab
Brad Richardson <brichardson@structint.com>
2018-03-21 20:55:26 -0700

    Make Task type an instance of ToJSON

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Main where
 
+import           Data.Aeson   (ToJSON)
 import           GHC.Generics (Generic)
 
 data Task = Task
     { taskId      :: Int
     , description :: String
     } deriving (Eq, Show, Generic)
+
+instance ToJSON Task
 
 main = putStrLn "Hello"
