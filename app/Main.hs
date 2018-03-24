6e718de64c4c7b881c5cbb667d0bfcd8a9d1755b
Brad Richardson <brichardson@structint.com>
2018-03-23 19:05:24 -0700

    Create some initial tasks

--- app/Main.hs
+++ app/Main.hs
@@ -18,4 +18,12 @@ type HelmAPI =
         Get '[JSON] [Task]
     :<|> Capture "taskId" Int :> Get '[JSON] Task
 
+tasks :: [Task]
+tasks =
+    [ Task 1 "Say hello"
+    , Task 2 "Say goodbye"
+    , Task 3 "Make stuff up"
+    , Task 4 "Foobar"
+    ]
+
 main = putStrLn "Hello"
